08b69ef3c8e0432e061c140fdbf694ef77019a40
denise <denise.tsui271@gmail.com>
2023-03-22 17:26:27 -0400

    test for straight movement main code

---
+++ main/test_straight/motor_defs.h
@@ -0,0 +1,7 @@
+void motor_setup();
+void straight_move(int speed, bool direction);
+void turn_move(int speed, bool direction);
+void stop_motors();
+void straight_vary(int speedA, int speedB, bool direction);
+void driveLeft(int speed);
+void driveRight(int speed);

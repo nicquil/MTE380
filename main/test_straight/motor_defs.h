eca3f8b4fb26617a675c4a8b18e85b32632965a8
denise <denise.tsui271@gmail.com>
2023-03-23 18:25:20 -0400

    remove unnecessary code and include new motor code

--- main/test_straight/motor_defs.h
+++ main/test_straight/motor_defs.h
@@ -1,7 +1,4 @@
 void motor_setup();
-void straight_move(int speed, bool direction);
-void turn_move(int speed, bool direction);
+void move_forward();
 void stop_motors();
-void straight_vary(int speedA, int speedB, bool direction);
-void driveLeft(int speed);
-void driveRight(int speed);
+void move_backward();

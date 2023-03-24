88ff50c8ce26a5a1d2a6abf865e02243b471d021
rnquan <73045670+rnquan@users.noreply.github.com>
2023-03-23 21:52:06 -0400

    added turning test and new header files for test
    edited gyro cpp, motor functions cpp and .ino

---
+++ main/turningTestAndOldMain/motor_defs.h
@@ -0,0 +1,6 @@
+void motor_setup();
+void move_forward();
+void stop_motors();
+void move_backward();
+void driveLeft(int speed);
+void driveRight(int speed);
---
+++ main/turningTestAndOldMain/sensor_functions.h
@@ -0,0 +1,28 @@
+/*!
+ * @file sensor_functions.h
+ * @brief Compilation of sensor function prototypes
+ *
+ * @author 
+ * @version  V1.0
+ * @date  2023-03-17
+ */
+
+#ifndef sensor_functions_h
+#define sensor_functions_h
+
+// setup serial communication and join i2c bus
+void setup_serial_i2c();
+
+// setup ultrasonic srf02 sensor
+void setupUltrasonic();
+
+// setup tof vl53l0x sensor
+void setupTOF();
+
+// read nnn cm of ultrasonic sensor distance detection
+int readUltrasonic();
+
+// read nnnn.nn mm of TOF sensor distance detection
+int readTOF();
+
+#endif
